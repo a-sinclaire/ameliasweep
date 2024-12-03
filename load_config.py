5dd1960c01116a9228128773c5476f17bbee82cc
Amelia Sinclaire <asinclaire.hri@gmail.com>
2024-12-03 14:25:54 -0500

    If no config exists and no template exists, generate a default config file.

--- load_config.py
+++ load_config.py
@@ -27,11 +27,100 @@ def load_config() -> dict:
 def generate_new_config() -> dict:
     if os.path.isfile('config_template.yaml'):
         shutil.copy('config_template.yaml', config_path)
-    else:
-        raise NotImplementedError
+        with open(config_path, 'r') as f:
+            return yaml.safe_load(f)
+
+    config = default_config()
+    with open(config_path, 'w') as outfile:
+        yaml.dump(config, outfile, sort_keys=False)
 
     with open(config_path, 'r') as f:
         return yaml.safe_load(f)
+
+
+def default_config() -> dict:
+    return {'CONTROLS': {'KEYBOARD': {'LEFT': 'KEY_LEFT',
+                                      'RIGHT': 'KEY_RIGHT',
+                                      'UP': 'KEY_UP',
+                                      'DOWN': 'KEY_DOWN',
+                                      'REVEAL': ' ',
+                                      'FLAG': 'f',
+                                      'RESET': 'r',
+                                      'HOME': 'KEY_HOME',
+                                      'END': 'KEY_END',
+                                      'CEILING': 'KEY_PPAGE',
+                                      'FLOOR': 'KEY_NPAGE',
+                                      'HELP': 'h',
+                                      'HIGHSCORES': 'p',
+                                      'EXIT': 'q'
+                                      },
+                         'MOUSE': {'LEFT': None,
+                                   'RIGHT': None,
+                                   'UP': None,
+                                   'DOWN': None,
+                                   'REVEAL': 'BUTTON1_CLICKED',
+                                   'FLAG': 'BUTTON3_CLICKED',
+                                   'RESET': None,
+                                   'HOME': None,
+                                   'END': None,
+                                   'CEILING': None,
+                                   'FLOOR': None,
+                                   'HELP': None,
+                                   'HIGHSCORES': None,
+                                   'EXIT': None}},
+            'SETUP': {'NO_FLASH': False,
+                      'MIN_WIDTH': 2,
+                      'MIN_HEIGHT': 2,
+                      'MAX_WIDTH': None,
+                      'MAX_HEIGHT': None,
+                      'BEGINNER': {'WIDTH': '09',
+                                   'HEIGHT': '09',
+                                   'RATIO': 0.123},
+                      'INTERMEDIATE': {'WIDTH': 16,
+                                       'HEIGHT': 16,
+                                       'RATIO': 0.156},
+                      'EXPERT': {'WIDTH': 30,
+                                 'HEIGHT': 16,
+                                 'RATIO': 0.206}},
+            'HIGHSCORES': {'MAX_NAME_LENGTH': 6,
+                           'BEGINNER_MAX': 10,
+                           'INTERMEDIATE_MAX': 10,
+                           'EXPERT_MAX': 10,
+                           'CUSTOM_MAX': 10},
+            'LOOK': {'SYMBOLS': {'BLANK': ' ',
+                                 'ONE': '1',
+                                 'TWO': '2',
+                                 'THREE': '3',
+                                 'FOUR': '4',
+                                 'FIVE': '5',
+                                 'SIX': '6',
+                                 'SEVEN': '7',
+                                 'EIGHT': '8',
+                                 'MINE': '¤',
+                                 'FLAG': '¶',
+                                 'UNOPENED': '■'},
+                     'COLORS': {'DEFAULT': {'ONE': 4,
+                                            'TWO': 2,
+                                            'THREE': 1,
+                                            'FOUR': 3,
+                                            'FIVE': 5,
+                                            'SIX': 6,
+                                            'SEVEN': 0,
+                                            'EIGHT': 7,
+                                            'SELECTOR': 3,
+                                            'LOSE': 1,
+                                            'WIN': 2},
+                                'RGB': {'ONE': [0, 0, 1000],
+                                        'TWO': [0, 500, 0],
+                                        'THREE': [1000, 0, 0],
+                                        'FOUR': [0, 0, 500],
+                                        'FIVE': [500, 0, 0],
+                                        'SIX': [0, 500, 500],
+                                        'SEVEN': [0, 0, 0],
+                                        'EIGHT': [500, 500, 500],
+                                        'SELECTOR': [1000, 1000, 0],
+                                        'LOSE': [1000, 0, 0],
+                                        'WIN': [0, 1000, 0]}}}}
 
 
 def fill_uninitialized_values(config: dict) -> dict:
